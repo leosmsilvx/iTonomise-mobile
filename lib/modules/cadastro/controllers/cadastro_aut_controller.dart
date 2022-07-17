0b98ed54e0ae98d754f590daa2e73cf064ebfa75
leosmsilvx <leosmsilvx@gmail.com>
2022-07-17 14:57:03 -0300

    Criar Controllers

---
+++ lib/modules/cadastro/controllers/cadastro_aut_controller.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class CadastroAutController extends ChangeNotifier{
+
+}
---
+++ lib/modules/index/controllers/index_controller.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class IndexController extends ChangeNotifier{
+
+}
